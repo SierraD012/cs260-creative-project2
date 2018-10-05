b4bf681b3586713bf3a732815922564887644347
Sierra Davis <sd.birdlover@gmail.com>
2018-10-05 03:14:00 +0000

    Everything appears to be working, feel free to add stuff if you want

--- scripts.js
+++ scripts.js
@@ -1,26 +1,50 @@
-var tryCounter = 0; //use this and a mod function to pick what "wrong answer" response we give them
+//the button calls this function onclick
 function getQuestion(){
-    //get question via jquery/ajax
+    ajaxAPICall(function(data) {
+        console.log(">Ajax got: " + data);
+        var triviaPrompt = "Please give the question to the following Jeopardy answer to continue login: \n" + data[0] + ":";
+        
+        var userInput = prompt(triviaPrompt, "Your Answer");
+        
+        //get random message out of repsonses[]
+        var randomResponse = getRandResponse();
+        var incorrectMessage = randomResponse + "\n The correct answer was: " + data[1];
+        
+        alert(incorrectMessage);
+        
+        location.reload(true);
+    });
+}
+
+function getRandResponse(){
+    var responses = [ "Wrong again", "Wow, you really suck at this, don't you?",
+    "Do you even want to log in, bro?", "You make Internet Explorer look good.",
+    "My grandma could have answered that!", "#FAIL" ];
+
+    var min = 0;
+    var max = responses.length - 1;
+    var randIndex = Math.floor(Math.random() * (max - min)) + min;
+    
+    console.log("randIndex = " + randIndex);
+    return responses[randIndex];
+}
+
+
+function ajaxAPICall(callback){
     var apiURL = "http://jservice.io/api/random";
-    var triviaQ = ''; 
-
-    var response = prompt("Trivia question here:", "Your Answer");
-    //incorrect no matter what
-    alert("Wrong answer, please try again"); //rotate between answers
-    //go back to login and clear all fields they typed in
-}
-
-function clearFields(){
-}
-
-// $(document).ready(function() {
     
-//   $( "#loginBtn" ).click(function() {
-//       alert("Hey!");
-//       console.log("hey");
-//   });
-   
-   
-    
-// });
+      $.ajax({  
+            url: apiURL,
+            dataType: "json",
+            success: function(parsed_JSON) {
+                parsed_JSON = parsed_JSON[0];
+                console.log(parsed_JSON);
+                
+                var question = parsed_JSON['question'];
+                var answer = parsed_JSON['answer'];
+                var trivia = [question, answer];
 
+                callback(trivia);  //this returns trivia back to getQuestion()
+            }
+        });
+}

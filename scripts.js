a4d6b7b11b867f9f38a959d67c9c94d17966e142
Sierra Davis <sd.birdlover@gmail.com>
2018-10-05 02:00:51 +0000

    API call is working

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
-$(document).ready(function() {
+var tryCounter = 0; //use this and a mod function to pick what "wrong answer" response we give them
+function getQuestion(){
+    //get question via jquery/ajax
+    var apiURL = "http://jservice.io/api/random";
+    var triviaQ = ''; 
+
+    var response = prompt("Trivia question here:", "Your Answer");
+    //incorrect no matter what
+    alert("Wrong answer, please try again"); //rotate between answers
+    //go back to login and clear all fields they typed in
+}
+
+function clearFields(){
+}
+
+// $(document).ready(function() {
     
-   $( "#loginBtn" ).click(function() {
-       alert("Hey!");
-       console.log("hey");
-   });
+//   $( "#loginBtn" ).click(function() {
+//       alert("Hey!");
+//       console.log("hey");
+//   });
    
    
     
-});
+// });
+
